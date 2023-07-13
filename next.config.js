24a51c8f971a6a5f4eeb558ddd4853c8aadedb15
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-07-12 22:04:04 -0300

    refactor: remove useless config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const withTM = require('next-transpile-modules')(['three']);
+module.exports = withTM();
+
 const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com', 'avatar.vercel.sh']
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@tremor/react']
-  }
+  transpilePackages: ['three']
 };
 
 module.exports = nextConfig;
